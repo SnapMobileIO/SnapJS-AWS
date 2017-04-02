5f6a204217dd9cf5f2d8b2818caffb00f9fb6238
Catie Stallings <catiestallings@Caties-MacBook-Pro.local>
2017-04-02 13:41:27 -0500

    Fix aws helper import in index.js

--- src/aws/index.js
+++ src/aws/index.js
@@ -2,7 +2,7 @@
 
 import { Router } from 'express';
 import * as controller from './aws.controller';
-import { awsHelper as awsHelper } from 'snapjs-aws';
+import * as awsHelper from './aws.helper';
 import multer from 'multer';
 import multerS3 from 'multer-s3';
 import aws from 'aws-sdk';
