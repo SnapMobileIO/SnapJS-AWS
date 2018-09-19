702f478cb0b7fa134b2227d664a199409ead67fc
kadinvanvalin <kadin@Kadins-MBP.attlocal.net>
2018-09-19 13:34:54 -0500

    fix env

--- src/aws/aws.controller.js
+++ src/aws/aws.controller.js
@@ -8,7 +8,7 @@ const AWS_S3_FILES_BUCKET = process.env.AWS_S3_FILES_BUCKET;
 const AWS_S3_FILES_KEY_PREFIX = process.env.AWS_S3_FILES_KEY_PREFIX;
 const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
 const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
-const AWS_S3_VIDEO_OVERSIZED_PREFIX = process.env.AWS_S3_FILES_OVERSIZED_PREFIX
+const AWS_S3_VIDEO_OVERSIZED_PREFIX = process.env.AWS_S3_VIDEO_OVERSIZED_PREFIX
 
 export function uploadToAws(req, res, next) {
   let file = req.file;
