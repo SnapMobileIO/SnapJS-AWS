b55f695c3d2bd24dbbc32b80b55f47540cc966dd
kadinvanvalin <kadin@Kadins-MBP.attlocal.net>
2018-09-20 10:19:45 -0500

    fix build

--- dist/aws/aws.controller.js
+++ dist/aws/aws.controller.js
@@ -24,7 +24,7 @@ var AWS_S3_FILES_BUCKET = process.env.AWS_S3_FILES_BUCKET;
 var AWS_S3_FILES_KEY_PREFIX = process.env.AWS_S3_FILES_KEY_PREFIX;
 var AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
 var AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
-var AWS_S3_VIDEO_OVERSIZED_PREFIX = process.env.AWS_S3_FILES_OVERSIZED_PREFIX;
+var AWS_S3_VIDEO_OVERSIZED_PREFIX = process.env.AWS_S3_VIDEO_OVERSIZED_PREFIX;
 
 function uploadToAws(req, res, next) {
   var file = req.file;
